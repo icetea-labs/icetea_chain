1bedeb46a7ac2e0cd426a30588797d59e965c498
Truong Hong Thi <thi1981@gmail.com>
2019-03-11 02:32:48 +0700

    Fix ESLint

--- icetea/StateProxy.js
+++ icetea/StateProxy.js
@@ -209,7 +209,7 @@ const applyChanges = (stateTable, { deployedContracts, storages, balances }) => 
     // Because we deep cloned, should be ok with direct assignment, no need for _.merge?
     stateTable[addr].storage = storages[addr]
   })
-  
+
   if (balances) {
     Object.keys(balances).forEach(addr => {
       const value = balances[addr]
