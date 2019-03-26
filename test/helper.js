4b54f38cbccd37e23c3f55d2633e81f1eea90592
hoanghuy226 <hoanghuy226@gmail.com>
2019-03-26 13:10:37 +0700

    init

--- test/helper.js
+++ test/helper.js
@@ -4,13 +4,13 @@ exports.randomAccountWithBalance = async (tweb3, intialBalance = 10000) => {
   // This is the only key which has initial balance as defined in config.
   // However, because test suites run in parallel, we won't use this key
   // directly in tests, otherwise balance checks might fail
-  const configKey = 'CJUPdD38vwc2wMC3hDsySB7YQ6AFLGuU6QYQYaiSeBsK'
-  const account = await tweb3.wallet.importAccount(configKey)
-  const from = account.account
+  // const configKey = 'CJUPdD38vwc2wMC3hDsySB7YQ6AFLGuU6QYQYaiSeBsK'
+  // const account = await tweb3.wallet.importAccount(configKey)
+  // const from = account.account +'a'
   const keyInfo = await ecc.newKeyPairWithAddress()
 
   // send money from configKey to newKey
-  const result = await tweb3.sendTransactionCommit({ from: from, to: keyInfo.address, value: intialBalance })
+  const result = await tweb3.sendTransactionCommit({ from: 'tea_3M76NHiZ4ipvMmTVBVJ1Wj378RLJ', to: keyInfo.address, value: intialBalance })
 
   if (result.check_tx.code) {
     throw new Error('check_tx: ' + result.check_tx.code + ' - ' + result.check_tx.log)
