e463a7342f273f07d34ecfde99705bce2efcbbad
Sotatek-DucPham <duc.pham@sotatek.com>
2019-03-27 21:47:44 +0700

    ICETEA_ENDPOINT

--- web/tweb3.js
+++ web/tweb3.js
@@ -2,4 +2,4 @@
 import { IceTeaWeb3 } from 'icetea-web3'
 // export default window.tweb3 = new IceTeaWeb3('http://localhost:3001/api');
 // let socket = process.env.NODE_ENV === 'production' ? 'ws://178.128.58.128/websocket' : 'ws://localhost:26657/websocket'
-export default window.tweb3 = new IceTeaWeb3(process.env.ICETEA_WS)
+export default window.tweb3 = new IceTeaWeb3(process.env.ICETEA_ENDPOINT)
