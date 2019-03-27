53471c92c6aa35a015f229ffbc6e0974bbfc041b
Truong Hong Thi <thi1981@gmail.com>
2019-03-27 19:55:57 +0700

    Edit comments

--- web/tweb3.js
+++ web/tweb3.js
@@ -1,5 +1,8 @@
 // import { IceTeaWeb3 } from '../tweb3'
 import { IceTeaWeb3 } from 'icetea-web3'
-// export default window.tweb3 = new IceTeaWeb3('http://localhost:3001/api');
-// let socket = process.env.NODE_ENV === 'production' ? 'ws://178.128.58.128/websocket' : 'ws://localhost:26657/websocket'
+
+// For localhost test, set .env.dev ICETEA_WS = http://localhost:3001/api or ws://localhost:3001/websocket
+
+// NOTE: don't commit public IP to github, even in comment. Put it in env files.
+
 export default window.tweb3 = new IceTeaWeb3(process.env.ICETEA_WS)
