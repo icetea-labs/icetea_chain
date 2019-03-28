25e67c12f0ec883ca1b156d61103da137bdebb0c
Truong Hong Thi <thi1981@gmail.com>
2019-03-28 18:58:29 +0700

    djs to compile into rjs

--- icetea/vm/js/babel/raw/index.js
+++ icetea/vm/js/babel/raw/index.js
@@ -1,3 +1,5 @@
 const loop = require('../loopEntryGuard')
 const func = require('../functionEntryGuard')
-module.exports = babel => [loop(babel), func(babel)]
+const dynamic = require('../dynamicCodeGuard')
+
+module.exports = babel => [dynamic(babel), loop(babel), func(babel)]
