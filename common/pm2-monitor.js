5b881dba78f54fcca8be31dd5fda90fac9d644f4
Sotatek-DucPham <duc.pham@sotatek.com>
2019-04-04 09:22:25 +0700

    pm2 update

---
+++ common/pm2-monitor.js
@@ -0,0 +1,18 @@
+const pm2 = require('pm2')
+const iceteaName = process.env.ICETEA_NAME || 'icetea'
+const tendermintName = process.env.TENDERMINT_NAME || 'tendermint'
+
+pm2.launchBus(function (err, bus) {
+  if (err) {
+    console.error(err)
+  }
+  bus.on('process:event', function (data) {
+    if (data.event === 'exit') {
+      const name = data.process.name
+      if (name === iceteaName) {
+        console.log('Ice Tea is exitting, send SIGTERM to tendermint')
+        pm2.sendSignalToProcessName('SIGTERM', tendermintName)
+      }
+    }
+  })
+})
