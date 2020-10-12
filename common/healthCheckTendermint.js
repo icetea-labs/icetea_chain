6c69ba46654690893a8582f2b82bdc25dad232f9
Tung <thanhtung6824@gmail.com>
2020-10-12 10:28:04 +0700

    change to local call

--- common/healthCheckTendermint.js
+++ common/healthCheckTendermint.js
@@ -2,7 +2,7 @@ const axios = require('./axios')
 
 exports.healthCheck = async () => {
   try {
-    return await axios.get('http://rpc.icetea.io/health')
+    return await axios.get('http://localhost:26657/health')
   } catch (error) {
     return Promise.reject(error)
   }
