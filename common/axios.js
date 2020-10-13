0162361f4a40ea9c39d973c5d8b1633678e8f018
Tung <thanhtung6824@gmail.com>
2020-10-13 16:43:56 +0700

    add axios timeout

--- common/axios.js
+++ common/axios.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
 const pm2 = require('pm2')
 const tendermintName = process.env.TENDERMINT_NAME || 'tendermint'
+const axiosTimeout = process.env.AXIOS_TIMEOUT || 5000
 const { sendDirect } = require('./send')
 const debug = require('debug')('icetea:monitor')
 
 let timeOut = 0
-const httpClient = axios.create({ timeout: 5000 })
+const httpClient = axios.create({ timeout: axiosTimeout })
 
 httpClient.interceptors.response.use(response => response, async error => {
   if (error.code === 'ECONNABORTED') {
