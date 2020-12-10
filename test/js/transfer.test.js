cf9a43deebf34f50798aea798259ab8cb404b2ba
Tung <thanhtung6824@gmail.com>
2020-12-10 14:08:24 +0700

    add UT

--- test/js/transfer.test.js
+++ test/js/transfer.test.js
@@ -104,4 +104,16 @@ describe('transfer', () => {
     const result = await tweb3.sendTransactionCommit({ from: from, to, value, fee })
     expect(result.deliver_tx.code).toBeFalsy()
   })
+
+  test('transfer with same transaction will throw err', async () => {
+    const { privateKey, address: from } = account10k
+    tweb3.wallet.importAccount(privateKey)
+    const keyInfo = await ecc.newBankKeys()
+
+    const tx = { from, to: keyInfo.address, value: 1000 }
+    const txSigned = await tweb3.signTransaction(tx, { from, nonce: 1 })
+
+    await tweb3.sendTransaction(txSigned, { from, nonce: 1 }, 'commit')
+    await expect(tweb3.sendTransaction(txSigned, { from, nonce: 1 }, 'commit')).rejects.toThrowError('This transaction was already included in blockchain, no need to send again.')
+  })
 })
